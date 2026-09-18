3391b6b52c12a0f0e59a037acec7711af4003c2a
digitaldotsolution <digitaldotsolution@users.noreply.github.com>
2026-09-18 16:00:33 +0500

    feat: move manufacturing boxes below Sole Types & Formulations

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -23,8 +23,8 @@ export default function Home() {
             <HeroArea />
             <ShoeSoleScroller />
             <AboutArea />
-            <ResumeArea />
             <ServiceArea />
+            <ResumeArea />
             <BlogArea />
             <TestimonoalArea />
             <PortfolioArea />
